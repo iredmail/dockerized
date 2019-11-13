6dc23e0e3585bf69fc43b7266f0cdcda4b707941
Zhang Huangbin <zhb@iredmail.org>
2019-11-13 14:16:11 +0800

    Generate my.cnf. Move all Dockerfiles to Dockerfiles/.

--- build.sh
+++ build.sh
@@ -1,1 +1,22 @@
-docker build --tag iredmail-mariadb:dev .
+#!/usr/bin/env bash
+# Usage:
+#
+#   ./build.sh          # Build with `Dockerfiles/Dockerfile`
+#   ./build.sh <name>   # name of file under `Dockerfiles/`.
+
+f="$1"
+shift
+
+if [[ X"${f}" == X"" ]]; then
+    df="Dockerfiles/Dockerfile"
+    label="iredmail-core"
+else
+    df="Dockerfiles/${f}"
+    label="iredmail-${f}"
+fi
+
+[[ -f ${df} ]] || (echo "Docker file ${df} doesnt exist." && exit 255)
+
+docker build \
+    --tag ${label}:dev \
+    -f ${df} .
