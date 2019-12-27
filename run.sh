797b6cc85e874d02854a94fb25c592af3397dab9
Zhang Huangbin <zhb@iredmail.org>
2019-12-27 08:53:23 +0800

    Add volume for backup.

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ for dir in ${DOCKER_VOLUME_BASEDIR} \
     ${DOCKER_VOLUME_CUSTOM_CONF_DIR} \
     ${DOCKER_VOLUME_DATA_DIR} \
     ${DOCKER_VOLUME_SSL_DIR} \
+    ${DOCKER_VOLUME_BACKUP_DIR} \
     ${DOCKER_VOLUME_MAILBOXES_DIR} \
     ${DOCKER_VOLUME_MYSQL_DATA_DIR} \
     ${DOCKER_VOLUME_CLAMAV_DB_DIR} \
@@ -29,6 +30,7 @@ docker run \
     -v ${DOCKER_VOLUME_CUSTOM_CONF_DIR}:/opt/iredmail/custom \
     -v ${DOCKER_VOLUME_DATA_DIR}:/opt/iredmail/data \
     -v ${DOCKER_VOLUME_SSL_DIR}:/opt/iredmail/ssl \
+    -v ${DOCKER_VOLUME_BACKUP_DIR}:/var/vmail/backup \
     -v ${DOCKER_VOLUME_MAILBOXES_DIR}:/var/vmail/vmail1 \
     -v ${DOCKER_VOLUME_MYSQL_DATA_DIR}:/var/lib/mysql \
     -v ${DOCKER_VOLUME_CLAMAV_DB_DIR}:/var/lib/clamav \
