cafae2cf8f433fc60bd7eb26f34c73b6235987cb
Zhang Huangbin <zhb@iredmail.org>
2022-03-20 21:27:06 +0800

    Install iptables (required by fail2ban). fixes #102

--- scripts/install_all_pkgs.sh
+++ scripts/install_all_pkgs.sh
@@ -23,7 +23,7 @@ PKGS_IREDAPD="python3-sqlalchemy python3-dnspython python3-pymysql python3-ldap 
 PKGS_IREDADMIN="python3-jinja2 python3-netifaces python3-bcrypt python3-dnspython python3-simplejson python3-more-itertools"
 PKGS_MLMMJ="mlmmj altermime"
 PKGS_MLMMJADMIN="uwsgi uwsgi-plugin-python3 python3-requests python3-pymysql python3-psycopg2 python3-ldap python3-more-itertools"
-PKGS_FAIL2BAN="fail2ban bind9-dnsutils"
+PKGS_FAIL2BAN="fail2ban bind9-dnsutils iptables"
 PKGS_ROUNDCUBE="php-bz2 php-curl php-gd php-imap php-intl php-json php-ldap php-mbstring php-mysql php-pgsql php-pspell php-xml php-zip mcrypt mariadb-client aspell"
 PKGS_ALL="wget gpg-agent supervisor mailutils python3-pip less vim-tiny
     ${PKGS_BASE}
