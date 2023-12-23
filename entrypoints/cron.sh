3d430a39c24630d4698d295dba987bc24fc2e9fe
Steve Myers <35839355+smyers119@users.noreply.github.com>
2023-12-23 16:08:30 -0500

    fix cron bug
    Fix cron to conform with iredmail documentation here: https://docs.iredmail.org/letsencrypt.html

--- entrypoints/cron.sh
+++ entrypoints/cron.sh
@@ -8,7 +8,7 @@
 
 . /docker/entrypoints/functions.sh
 
-echo "1 3 * * * certbot renew --webroot -w /var/www/html --post-hook '/usr/sbin/service postfix restart; /usr/sbin/service nginx restart; /usr/sbin/service dovecot restart'" > /etc/cron.d/letsencrypt
+echo "1 3 * * * certbot renew --webroot -w /var/www/html --post-hook 'ln -sf /etc/letsencrypt/live/${HOSTNAME}/privkey.pem /opt/iredmail/ssl/key.pem; /usr/sbin/service postfix restart; /usr/sbin/service nginx restart; /usr/sbin/service dovecot restart'" > /etc/cron.d/letsencrypt
 
 chmod 0644 /etc/cron.d/letsencrypt
 
